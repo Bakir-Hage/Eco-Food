e722bdd6ccf26ade97e665e7232009b42f9dd949
Bakir-Hage <Bakir.ah2@gmail.com>
2026-04-05 05:16:52 +0300

    feat: add landing page components including Header, HeroSection, FeaturesSection, and NotFoundPage
    - Implemented FeatureCard component to showcase features of Eco-Food.
    - Created FeaturesSection to organize and display multiple FeatureCards.
    - Developed Header for navigation with logo and title.
    - Added HeroSection for introductory content and call-to-action buttons.
    - Integrated all components into LandingPage for a cohesive layout.
    - Created NotFoundPage to handle 404 errors with a user-friendly message and navigation link.

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="absolute inset-0 z-0">
+      <nav className="absolute flex items-center gap-4 px-6 py-10">
+        <img
+          src="src/assets/Logo.png"
+          alt="Eco-Food Logo"
+          className="w-25 h-25"
+        />
+        <span className="font-semibold text-[35px] text-white">Eco-Food</span>
+      </nav>
+    </div>
+  );
+}
